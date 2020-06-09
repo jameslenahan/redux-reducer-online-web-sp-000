6ee56bc51fce9461d8634d5e0502b5fa7266d42a
James Lenahan <james.k.lenahan@gmail.com>
2020-06-09 14:20:50 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,5 +1,11 @@
+let state = {numberOfPresents: 0}
 export function managePresents(state, action){
-  let numberOfPresents = 0 
-
-  }
+    switch(action.type) {
+        case "INCREASE":
+          return Object.assign({}, state, {
+            numberOfPresents: state.numberOfPresents + 1
+          });
+        default:
+            return state;
+    }
 }
